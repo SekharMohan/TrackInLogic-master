235a491e64301b37faa724e21018a55d32cc3b2e
SekharMohan <sekarmohan77@gmail.com>
2017-03-01 23:15:25 +0700

    Initial commit

---
+++ app/src/main/java/trackinlogic/trans/pss/com/trackinlogic/BaseActivity.java
@@ -0,0 +1,14 @@
+package trackinlogic.trans.pss.com.trackinlogic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        assert savedInstanceState!=null;
+        
+    }
+}
